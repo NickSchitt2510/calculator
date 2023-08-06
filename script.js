0e0a6c62fc8bb1cb4362dc3c13bd8c44c6930433
NickSchitt2510 <121604402+NickSchitt2510@users.noreply.github.com>
2023-08-06 16:40:13 +0800

    Create html for numbers and operators

---
+++ script.js
@@ -0,0 +1,35 @@
+// Basic math logic
+function add(num1, num2) {
+    return num1 + num2;
+}
+
+function subtract(num1, num2) {
+    return num1 - num2;
+}
+
+function multiply(num1, num2) {
+    return num1 * num2;
+}
+
+function divide(num1, num2) {
+    return num1 / num2;
+}
+
+// Operator
+
+let num1;
+let num2;
+let operator;
+
+function operate(num1, num2, operator) {
+    switch(operator) {
+        case '+':
+            return add(num1, num2);
+        case '-':
+            return subtract(num1, num2);
+        case '*':
+            return multiply(num1, num2);
+        case '/':
+            return divide(num1, num2);
+    }
+}
