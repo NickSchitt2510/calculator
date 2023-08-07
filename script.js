806a4a8da52a9d218f89eae2d47da68fb2bbbcca
NickSchitt2510 <121604402+NickSchitt2510@users.noreply.github.com>
2023-08-07 17:58:52 +0800

    Create initiateEquation work like a calculator and it works once

--- script.js
+++ script.js
@@ -19,10 +19,10 @@ function divide(num1, num2) {
 
 let num1;
 let num2;
-let operator;
+let symbol;
 
-function operate(num1, num2, operator) {
-    switch(operator) {
+function operate(num1, num2, symbol) {
+    switch(symbol) {
         case '+':
             return add(num1, num2);
         case '-':
@@ -34,22 +34,72 @@ function operate(num1, num2, operator) {
     }
 }
 
-let displayVal = [];
+// Store the whole equation
+let displayText = '';
+let numberVal = [];
+let equationCounter = 0;
 
 function populateDisplay() {
-    const display = document.querySelector('#display');
+    const display = document.querySelector('#resultDisplay');
 
     const numbers = document.querySelectorAll('.number');
-    console.log(numbers);
     numbers.forEach(number => {
         number.addEventListener('click', () => {
-        displayVal.push(number.getAttribute('value'));
-        display.textContent = displayVal.join('');
-    })});
+            numberVal.push(number.getAttribute('value'));
+            display.textContent = numberVal.join('');
+        });
+    });
 }
 
-function updateDisplayVal() {
-    
+function initiateEquation() {
+    equationCounter = 0;
+    const display = document.querySelector('#resultDisplay');
+    const equation = document.querySelector('#equationDisplay');
+
+    const operators = document.querySelectorAll('.operator');
+    operators.forEach(operator => {
+        operator.addEventListener('click', () => {
+            if (equationCounter === 0) {
+                // Store the array to num1
+                num1 = +numberVal.join('');
+                console.log(num1);
+                // Store the operator to operator
+                symbol = operator.getAttribute('value');
+                console.log(symbol);
+                // change textContent to display
+                displayText += `${num1} ${symbol} `;
+                console.log(displayText);
+                display.textContent = displayText;
+                numberVal = [];
+                equationCounter++;
+            } else {
+                num2 = +numberVal.join('');
+                console.log(num2);
+                // Operate with old symbol and new num
+                num1 = operate(num1, num2, symbol);
+                display.textContent = num1;
+                // Operate first then update the symbol
+                
+                // Store the new symbol
+                symbol = operator.getAttribute('value');
+                console.log(symbol);
+                
+                // Display the new symbol in the equation
+                displayText += `${num2} ${symbol} `;
+                console.log(displayText);
+
+                // change textContent to display
+                equation.textContent = displayText;
+                numberVal = [];
+                equationCounter++;
+            }
+        });
+    });
+}
+
+function continueEquation() {
+
 }
 
 populateDisplay();
+initiateEquation();
